baf387ee52adac8d28c7a985e5e85b003154d795
and0429 <106358247@qq.com>
2015-12-20 22:14:56 +0800

    d

--- app/js/route.js
+++ app/js/route.js
@@ -3,22 +3,28 @@
 ShareApp.config(['$stateProvider', '$urlRouterProvider', 
 	function($stateProvider, $urlRouterProvider) {
 
-		$urlRouterProvider.otherwise('/state1');	
+		$urlRouterProvider.otherwise('/index');	
 
 
 		$stateProvider
+    .state(
+      'index', {
+        url: '/index',
+        templateUrl: 'tpls/index.html'
+      }
+    )
 		.state(
 			'state1', {
-     					url: "/state1",
-      					templateUrl: "tpls/123.html"
-    				  }
-    	)
-    	.state(
+     		url: "/state1",
+      	templateUrl: "tpls/123.html"
+    		}
+    )
+    .state(
 			'state2', {
-     					url: "/state2",
-      					templateUrl: "tpls/456.html"
-    				  }
-    	)
+     		url: "/state2",
+      	templateUrl: "tpls/456.html"
+    		}
+    )
 
 
 
